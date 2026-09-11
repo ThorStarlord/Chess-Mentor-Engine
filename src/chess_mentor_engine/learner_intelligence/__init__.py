7d0a2a8ef6315371c14ed63f81c47fc0868d88b0
ThorStarlord <140429883+ThorStarlord@users.noreply.github.com>
2026-09-11 09:41:54 -0300

    Export M40 next-session planner

--- src/chess_mentor_engine/learner_intelligence/__init__.py
+++ src/chess_mentor_engine/learner_intelligence/__init__.py
@@ -10,14 +10,38 @@ from .evidence_synthesis import (
     build_hypothesis_evidence_synthesis,
     validate_hypothesis_evidence_synthesis,
 )
+from .next_session import (
+    NEXT_SESSION_PLAN_SCHEMA_VERSION,
+    NEXT_SESSION_POLICY_SCHEMA_VERSION,
+    NextSessionAction,
+    NextSessionActionCandidate,
+    NextSessionPlan,
+    NextSessionPolicy,
+    NextSessionPolicyRef,
+    build_default_next_session_policy,
+    build_next_session_plan,
+    validate_next_session_plan,
+    validate_next_session_policy,
+)
 
 __all__ = [
     "HYPOTHESIS_EVIDENCE_SYNTHESIS_SCHEMA_VERSION",
+    "NEXT_SESSION_PLAN_SCHEMA_VERSION",
+    "NEXT_SESSION_POLICY_SCHEMA_VERSION",
     "EvidenceSynthesisReference",
     "HypothesisEvidenceCounts",
     "HypothesisEvidenceSynthesis",
     "HypothesisEvidenceUnitSynthesis",
     "HypothesisReviewSummary",
+    "NextSessionAction",
+    "NextSessionActionCandidate",
+    "NextSessionPlan",
+    "NextSessionPolicy",
+    "NextSessionPolicyRef",
+    "build_default_next_session_policy",
     "build_hypothesis_evidence_synthesis",
+    "build_next_session_plan",
     "validate_hypothesis_evidence_synthesis",
+    "validate_next_session_plan",
+    "validate_next_session_policy",
 ]
