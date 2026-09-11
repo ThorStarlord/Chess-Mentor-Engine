37fe7d8262184a5f947d0b838b4ee1bdc8862ac5
ThorStarlord <140429883+ThorStarlord@users.noreply.github.com>
2026-09-11 07:54:17 -0300

    Export chess knowledge assertion surface

--- src/chess_mentor_engine/chess_knowledge/__init__.py
+++ src/chess_mentor_engine/chess_knowledge/__init__.py
@@ -1,5 +1,20 @@
 """Chess Knowledge Ontology public surface."""
 
+from .assertions import (
+    KNOWLEDGE_ASSERTION_BUNDLE_SCHEMA_VERSION,
+    KNOWLEDGE_ASSERTION_SCHEMA_VERSION,
+    KnowledgeAssertion,
+    KnowledgeAssertionBundle,
+    KnowledgeAssertionRef,
+    KnowledgeEvidenceRef,
+    KnowledgeProvenance,
+    KnowledgeQualifier,
+    KnowledgeSubject,
+    build_assertion_bundle,
+    build_knowledge_assertion,
+    validate_assertion_bundle,
+    validate_knowledge_assertion,
+)
 from .fingerprints import concept_fingerprint, ontology_fingerprint
 from .model import (
     AuthorityClass,
@@ -13,6 +28,7 @@ from .model import (
 )
 from .registry import (
     DEFAULT_ONTOLOGY_RESOURCE,
+    DEFAULT_ONTOLOGY_RESOURCES,
     ONTOLOGY_SCHEMA_VERSION,
     OntologyLoadError,
     OntologyRegistry,
@@ -25,15 +41,29 @@ __all__ = [
     "ConceptKind",
     "ConceptRelationship",
     "DEFAULT_ONTOLOGY_RESOURCE",
+    "DEFAULT_ONTOLOGY_RESOURCES",
     "DetectionSupport",
     "ExternalMapping",
+    "KNOWLEDGE_ASSERTION_BUNDLE_SCHEMA_VERSION",
+    "KNOWLEDGE_ASSERTION_SCHEMA_VERSION",
+    "KnowledgeAssertion",
+    "KnowledgeAssertionBundle",
+    "KnowledgeAssertionRef",
+    "KnowledgeEvidenceRef",
+    "KnowledgeProvenance",
+    "KnowledgeQualifier",
+    "KnowledgeSubject",
     "ONTOLOGY_SCHEMA_VERSION",
     "OntologyDocument",
     "OntologyLoadError",
     "OntologyRegistry",
     "OntologyValidationError",
     "PedagogyMetadata",
+    "build_assertion_bundle",
+    "build_knowledge_assertion",
     "concept_fingerprint",
     "ontology_fingerprint",
+    "validate_assertion_bundle",
+    "validate_knowledge_assertion",
     "validate_ontology",
 ]
